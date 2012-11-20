805e75bd1afd6d3926e507139ebeaea228f26a09
Christian Krause <kizkizzbangbang@googlemail.com>
2012-11-20 09:23:13 +0100

    swing client outline

---
+++ client-swing/src/main/scala/SwingClient.scala
@@ -0,0 +1,59 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                               *
+ *  Copyright  ©  2012  Christian Krause                                                         *
+ *                                                                                               *
+ *  Christian Krause  <christian.krause@ufz.de>                                                  *
+ *                    <kizkizzbangbang@googlemail.com>                                           *
+ *                                                                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                               *
+ *  This file is part of 'ClusterKit'.                                                           *
+ *                                                                                               *
+ *  This project is free software: you can redistribute it and/or modify it under the terms      *
+ *  of the GNU General Public License as published by the Free Software Foundation, either       *
+ *  version 3 of the License, or any later version.                                              *
+ *                                                                                               *
+ *  This project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;    *
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    *
+ *  See the GNU General Public License for more details.                                         *
+ *                                                                                               *
+ *  You should have received a copy of the GNU General Public License along with this project.   *
+ *  If not, see <http://www.gnu.org/licenses/>.                                                  *
+ *                                                                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+package ckit
+package client
+package swing
+
+import scala.swing._
+
+import akka.actor.ActorSystem
+
+object SwingClient extends SwingApplication {
+  val system = ActorSystem("ckit")
+
+  def startup(args: Array[String]) {
+    val top = new MainFrame(null) {
+      override def closeOperation() {
+        SwingClient.quit()
+      }
+    }
+
+    top.title = "ClusterKit"
+
+    val tabbed = new TabbedPane
+    top.contents = tabbed
+
+    tabbed.pages += new TabbedPane.Page("Welcome", new Label("... this is ClusterKit"))
+
+    top.pack()
+    top.visible = true
+  }
+
+  override def quit() {
+    system.shutdown()
+    sys.exit(0)
+  }
+}
